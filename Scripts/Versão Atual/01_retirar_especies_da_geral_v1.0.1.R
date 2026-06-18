5f21b7c4ed0231e4135980abe9e52df5dce79b38
Fernando Lima, D.Sc. <pardalismitis@users.noreply.github.com>
2026-06-18 10:48:31 -0300

    Add comment for Nicholas in R script

--- Scripts/Versão Atual/01_retirar_especies_da_geral_v1.0.1.R
+++ Scripts/Versão Atual/01_retirar_especies_da_geral_v1.0.1.R
@@ -1,6 +1,6 @@
 if (!require(pacman)) install.packages("pacman")
 pacman::p_load(readxl, openxlsx, dplyr, stringr)
-
+# oi Nicholas
 # teste
 
 # =========================================================
